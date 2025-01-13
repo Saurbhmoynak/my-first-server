0510ba34c369fb3aa4bb3974b28fcd20aa57c3d7
Saurbh Moynak <94280322+Saurbhmoynak@users.noreply.github.com>
2025-01-13 22:23:32 +0530

    github data added

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const port = 3000;
+
+const githubData = {
+  "name": "Saurbhmoynak",
+  "port":"3000"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -14,9 +21,21 @@ app.get('/heading', (req, res) => {
   res.send('<h1>This is server</h1>')
 })
 
-app.listen(process.env.PORT, () => {
+app.get('/github', (req, res) => {
+  res.json(githubData)
+})
+
+// only for localhost
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// only for env port
+// app.listen(process.env.PORT, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
 
 
 //Now you can deploy
